a94079efc6df9c7fbfd012a6cf10c4a3eb2f9003
Mark Matis <markmatis11@live.com>
2021-03-04 17:12:23 +0000

    quote generator now uses a servlet and json instead of just a hardcoded list, title changes colors

--- portfolio/src/main/webapp/serverletscripts.js
+++ portfolio/src/main/webapp/serverletscripts.js
@@ -5,3 +5,18 @@ async function showHardcodedString() {
   const dateContainer = document.getElementById('forServlet');
   dateContainer.innerHTML = textFromResponse;
 }
+
+async function getListOfQuotes(){
+  const responseFromServer = await fetch('/quotesServlet');
+
+  const quotesList = await responseFromServer.json();
+  //below ones won't represent in json, due to extremely special characters
+  quotesList.push("  ⠀⠰⡿⠿⠛⠛⠻⠿⣷  \n⠀⠀⠀⠀⠀⠀⣀⣄⡀⠀⠀⠀⠀⢀⣀⣀⣤⣄⣀⡀  \n⠀⠀⠀⠀⠀⢸⣿⣿⣷⠀⠀⠀⠀⠛⠛⣿⣿⣿⡛⠿⠷  \n⠀⠀⠀⠀⠀⠘⠿⠿⠋⠀⠀⠀⠀⠀⠀⣿⣿⣿⠇  \n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠁  \n\n⠀⠀⠀⠀⣿⣷⣄⠀⢶⣶⣷⣶⣶⣤⣀  \n⠀⠀⠀⠀⣿⣿⣿⠀⠀⠀⠀⠀⠈⠙⠻⠗  \n⠀⠀⠀⣰⣿⣿⣿⠀⠀⠀⠀⢀⣀⣠⣤⣴⣶⡄  \n⠀⣠⣾⣿⣿⣿⣥⣶⣶⣿⣿⣿⣿⣿⠿⠿⠛⠃  \n⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄  \n⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡁  \n⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁  \n⠀⠀⠛⢿⣿⣿⣿⣿⣿⣿⡿⠟  \n⠀⠀⠀⠀⠀⠉⠉⠉  .");
+  quotesList.push("Nai hiruvalyë Valimar. Ihírienyes. Nai elyë hiruva.");
+  
+  const quote = quotesList[Math.floor(Math.random() * quotesList.length)];
+
+  
+  const greetingContainer = document.getElementById('greeting-container');
+  greetingContainer.innerText = quote;
+}
